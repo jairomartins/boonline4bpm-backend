6d30626e3000d413c8e8192e8f7e2f80c97482ca
Jairo Martins <jairo7martins@gmail.com>
2023-01-27 20:00:52 +0044

    's'

--- src/http/route.js
+++ src/http/route.js
@@ -3,7 +3,7 @@ const boCont = require('../controllers/boletimController')
 
 function route (app){
     app.get('/',(req,res)=>{
-
+        console.log("run")
         res.send('it´s ok!')
     })
 }
