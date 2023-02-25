4410f98d217046589d251b88d7283b18d005fcc8
jmartins <jairo7martins@gmail.com>
2023-02-24 22:32:31 -0300

    configuração token

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require ('mongoose')
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect('mongodb://127.0.0.1:27017/bo4bpm')
+mongoose.connect('mongodb://127.0.0.1:27017')
 
 mongoose.Promise = global.Promise;
 
