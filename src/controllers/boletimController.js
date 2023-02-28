4b0291410d60bc7049b985ceb56d918135ba6c7d
jmartins <jairo7martins@gmail.com>
2023-02-28 00:16:58 -0300

    adicionando tratamento de exceções

--- src/controllers/boletimController.js
+++ src/controllers/boletimController.js
@@ -4,16 +4,15 @@ const { json } = require('body-parser')
 const Boletim = require('../model/boletim')
 
 // retorna uma lista com todos os boletins de ocorrências no banco de dados
-//
-//
-//
-
-
 
 exports.BoletimList = async (req, res)=>{
-    const result = await Boletim.find()
-    console.log(result)
-    res.send(JSON.stringify(result))
+    try{
+        const result = await Boletim.find()
+        return res.status(200).send(JSON.stringify(result))
+    }catch(err){
+        return res.status(500).send({message:"Erro não foi possivel buscar a lista de boletins", error:err})
+    }
+    
 }
 
 //buscar boletim no banco de dados de acordo com id repassado via get 
@@ -21,23 +20,30 @@ exports.BoletimList = async (req, res)=>{
 //
 //
 exports.boletimByID = async (req, res)=>{
-    const result = await Boletim.find({_id: req.params.IDBoletim})
-    console.log(req.params)
-    res.send(JSON.stringify(result))
+    try{
+        const result = await Boletim.find({_id: req.params.IDBoletim})
+        return res.status(200).send(JSON.stringify(result))
+    }catch(err){
+        return res.status(500).send({message:"Boletim não encontrado", error:err}) 
+    }
 }
 
 exports.boletimByNumero = async (req, res)=>{
-    const result = await Boletim.findOne({numero: req.params.NumeroBoletim}).sort({'_id':-1})
-    console.log(req.params)
-    console.log(JSON.stringify(result))
-    res.send(JSON.stringify(result))
+    try{
+        const result = await Boletim.findOne({numero: req.params.NumeroBoletim}).sort({'_id':-1})
+        return res.status(200).send(JSON.stringify(result))
+    }catch(err){
+        return res.status(500).send({message:"Boletim não encontrado", error:err}) 
+    }
 }
 
-exports.boletimByNumeroECidade = async (req, res)=>{
-    const result = await Boletim.findOne({numero: req.params.NumeroBoletim, municipio:req.params.cidade}).sort({'_id':-1})
-    console.log(req.params)
-    console.log(JSON.stringify(result))
-    res.send(JSON.stringify(result))
+exports.boletimByNumeroAndCidade = async (req, res)=>{
+    try {
+        const result = await Boletim.findOne({numero: req.params.NumeroBoletim, municipio:req.params.cidade}).sort({'_id':-1})
+        return res.status(200).send(JSON.stringify(result))
+    } catch (err) {
+        return res.status(500).send({message:"Boletim não encontrado", error:err})    
+    }    
 }
 
 // remove do banco de dados o boletim que tem o _id igual a repassado via get 
@@ -45,8 +51,12 @@ exports.boletimByNumeroECidade = async (req, res)=>{
 // 
 //
 exports.removeBoletimByID = async( req, res)=>{
-    const result = await Boletim.remove({_id: req.params.IDBoletim})
-    res.send(JSON.stringify(result))
+    try {
+        const result = await Boletim.remove({_id: req.params.IDBoletim})
+        return res.status(200).send(JSON.stringify(result))
+    } catch (err) {
+        return res.status(500).send({message:"Boletim não encontrado", error:err})     
+    } 
 }
 
 
@@ -55,32 +65,33 @@ exports.removeBoletimByID = async( req, res)=>{
 //
 //
 exports.createBoletim = async(req, res)=>{
-    Boletim.findOneAndUpdate(
-        {numero: req.body.boletim.numero, data : req.body.boletim.data},//critério de pesquisa. verifica se ja existe o boletim no banco de dados
-        {$set:req.body.boletim},
-        { upsert: true, new: true },
-        onBoletimUpdate
-    )
-}
-
-function onBoletimUpdate(err, boletim) {
-    if (err) throw err;
-    if (!boletim) {
-      console.log('Novo documento criado!');
-    } else {
-      console.log('Documento atualizado!');
+    try{
+        await Boletim.findOneAndUpdate(
+            {numero: req.body.boletim.numero, data : req.body.boletim.data},//critério de pesquisa. verifica se ja existe o boletim no banco de dados
+            {$set:req.body.boletim},
+            { upsert: true, new: true },
+        )
+        
+        res.status(200).send({message:"Boletim registrado com sucesso !"}) 
+    }catch(err){
+        return res.status(500).send({message:"Não foi possivel registrar o boletim, erro no servidor !", err:err})
     }
 }
 
+
 //Busca por Boletim que contem no efetivo ids igual do passado no params.id
 exports.listaMeusBos = async (req, res)=>{
-    const result = await Boletim.find({
-        efetivo:{$elemMatch:{
-            id:req.params.id
-        }}
-    })
-    console.log(result.numero)
-    res.status(200).send(JSON.stringify(result))
+    console.log(req.params.id)
+    try {
+        const result = await Boletim.find({
+            efetivo:{$elemMatch:{
+                id:req.params.id
+            }}
+        })
+        return res.status(200).send(result)     
+    } catch (err) {
+        return res.status(500).send({message:"Erro de ao solicitar lista de boletins deste usuario"})
+    }
 }
 
 
--- src/http/boletimRoute.js
+++ src/http/boletimRoute.js
@@ -30,7 +30,7 @@ function route (app){
     })
 
     app.get('/adm/listByNumeroECidade/:NumeroBoletim/:cidade',verificaToken , async(req, res) => {
-    await boletimController.boletimByNumeroECidade(req, res)
+    await boletimController.boletimByNumeroAndCidade(req, res)
     })
 
     app.post('/adm/salvarBoletim', async (req,res)=>{
@@ -38,25 +38,34 @@ function route (app){
     })
 
     app.post('/adm/boletim/create', async (req, res)=>{
-        // TODO - salvar boletim de ocorrencia de acordo com o numero repassado
+        await boletimController.createBoletim(req, res)
     })
 
-    app.post('/adm/boletim/search', async (req, res)=>{
-        // TODO - buscar boletim de ocorrencia de acordo com o numero repassado
+    app.post('/adm/boletim/search/:numero', async (req, res)=>{
+        // buscar boletim de ocorrencia de acordo com o numero repassado
+        await boletimController.boletimByNumero(req,res)
     })
 
     app.post('/adm/boletim/update', async (req, res)=>{
-        // TODO - atualizar boletim de ocorrencia de acordo com o numero repassado
+        // atualizar boletim de ocorrencia de acordo com o numero repassado
+        await boletimController.createBoletim(req,res)
     })
 
-    app.post('/adm/boletim/delete', async (req, res)=>{
+    app.post('/adm/boletim/delete/:id', async (req, res)=>{
         // TODO - remover boletim de ocorrencia de acordo com o numero repassado
+        await boletimController.removeBoletimByID(req,res)
+    })
+
+    // lista boletins de um usuario pelo id do policial empregado no efetivo
+    app.get('/adm/boletim/list/:id', async (req,res)=>{
+        await boletimController.listaMeusBos(req, res)
     })
 
     app.get('/adm/listaMeusBos/:id', async (req,res)=>{
         await boletimController.listaMeusBos(req, res)
     })
     
+
 
     app.get('/adm/countBoletins/', async (req,res)=>{
         await boletimController.countBoletim(req, res)
