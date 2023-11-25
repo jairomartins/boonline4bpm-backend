731858d2b9ab0819c28e20173c745362a4d4ccb4
jmartins <jairo7martins@gmail.com>
2023-11-25 08:31:22 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -19,7 +19,19 @@ require('./http/authRouter')(app);
 // Certificados SSL do Let's Encrypt
 const privateKeyPath = '/etc/letsencrypt/live/jmartins.vps-kinghost.net/privkey.pem';
 const certificatePath = '/etc/letsencrypt/live/jmartins.vps-kinghost.net/fullchain.pem';
+// Middleware para permitir requisições CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '167.249.15.184'); // ou substitua '*' pelo domínio específico que você deseja permitir
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
+    if (req.method === 'OPTIONS') {
+        // Preflight request. Reply successfully:
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 // Adicionar middleware para redirecionamento HTTP para HTTPS
 app.use((req, res, next) => {
     if (req.secure) {
