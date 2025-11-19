a795479088ba6fb70186b7ca629eb8090c64c75a
jmartins <jairo7martins@gmail.com>
2025-11-19 01:04:59 -0300

    token login 2h

--- src/lib/jwtconfig.js
+++ src/lib/jwtconfig.js
@@ -1,35 +1,31 @@
 const jwt = require('jsonwebtoken');
 const TOKEN_HEADER = "x-access-token";
-const SECRET_PASSWORD_TOKEN = process.env.SECRET_PASSWORD_TOKEN
+const SECRET_PASSWORD_TOKEN = process.env.SECRET_PASSWORD_TOKEN;
 
 const verificaToken = (req, res, next) => {
-    const token = req.headers[TOKEN_HEADER]
-    try{
-        if(!token){
-            return res.json({message:"você precisa de um token"})
-        }else{
-            jwt.verify(token,SECRET_PASSWORD_TOKEN, (err,decoded)=>{
-                if(err){
-                    console.log("Erro na verificação do token:", err);
-                    return res.status(401).json({ message: "Falha ao autenticar o token", auth:false});
-                    next()
-                }else{
-                    console.log("Token valido !")
-                    next()
-                }
-            })
-        }
-    }catch(err){
-        console.log("Erro ao verificar o token:", err);
-        return res.status(500).json({ message: "Erro ao verificar o token" });
+    const token = req.headers[TOKEN_HEADER];
+
+    if (!token) {
+        return res.status(401).json({ message: "Token não fornecido", auth: false });
     }
+
+    jwt.verify(token, SECRET_PASSWORD_TOKEN, (err, decoded) => {
+        if (err) {
+            console.log("Erro ao autenticar token:", err);
+            return res.status(401).json({ message: "Token inválido ou expirado", auth: false });
+        }
+
+        // Se chegou aqui, o token é válido
+        req.user = decoded;
+        next();
+    });
 };
 
-const gerarToken = (data)=>{
-    return jwt.sign({data},SECRET_PASSWORD_TOKEN)
-}
+const gerarToken = (data) => {
+    return jwt.sign({ data }, SECRET_PASSWORD_TOKEN, { expiresIn: "2h" }); // <-- defina um tempo de expiração aqui
+};
 
 module.exports = {
-  verificaToken,
-  gerarToken
+    verificaToken,
+    gerarToken
 };
