b458abb03819696e6d05c824e5044e52acb2c25a
jmartins <jairo7martins@gmail.com>
2025-11-25 02:43:46 -0300

    version

--- src/http/route.js
+++ src/http/route.js
@@ -1,7 +1,7 @@
 function route (app){
     app.get('/',(req,res)=>{
         console.log("[Servidor : ON]")
-        res.send('[Servidor : ON version 10/09/2025]') 
+        res.send('[Servidor : ON version 20/11/2025]') 
     })
 }
 module.exports = route
